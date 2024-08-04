9d81d212ebf219f03a006367c94d7c32a34ef014
ShpatV <shpatvata1@gmail.com>
2024-08-04 19:02:30 +0200

    KAN-39 implememt booking service with rental relationship

---
+++ src/main/java/com/orion/repository/BookingRepository.java
@@ -0,0 +1,34 @@
+package com.orion.repository;
+
+import com.orion.entity.Booking;
+import com.orion.entity.Rental;
+import com.orion.enums.vehicle.RentalStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    @Query("SELECT b FROM Booking b WHERE b.customer.id = :customerId")
+    List<Booking> findByCustomerId(@Param("customerId") Long customerId);
+
+    @Query("SELECT b FROM Booking b WHERE b.vehicle.id = :vehicleId")
+    List<Booking> findByVehicleId(@Param("vehicleId") Long vehicleId);
+
+    @Query("SELECT b FROM Booking b WHERE b.status = 'PENDING' AND " +
+            "(b.startDate <= :endDate AND b.endDate >= :startDate)")
+    List<Booking> findActiveBookings(@Param("startDate") LocalDateTime startDate,
+                                     @Param("endDate") LocalDateTime endDate);
+
+    @Query("Select b from Booking b where b.vehicle.id = :vehicleId and b.status = 'PENDING' and b.vehicleStatus = 'RESERVED' and " +
+            "(b.startDate <= :endDate AND b.endDate >= :startDate)")
+    List<Booking> findBookingsByVehicleAndDateRange(Long vehicleId, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("Select b from Booking b where b.status = :status ")
+    List<Booking> findByStatus(@Param("status") RentalStatus status);
+
+
+}
