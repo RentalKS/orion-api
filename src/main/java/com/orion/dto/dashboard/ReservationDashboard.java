fdd385b532ebb13499328987f41598e27b28c595
ShpatV <shpatvata1@gmail.com>
2024-08-08 20:52:56 +0200

    Signature and reservation

---
+++ src/main/java/com/orion/dto/dashboard/ReservationDashboard.java
@@ -0,0 +1,20 @@
+package com.orion.dto.dashboard;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReservationDashboard {
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private Long customerId;
+    private Long totalReservations;
+    private Double totalAmount;
+    private Long completedReservations;
+    private Long pendingReservations;
+    private Long canceledReservations;
+}
